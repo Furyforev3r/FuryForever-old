682c63581ef8a49fcd5934b73067ee7e5164ffb6
FuryForever <messiasfuryforever@gmail.com>
2023-09-20 19:04:16 -0300

    services export

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -3,6 +3,90 @@ interface Project {
     Description: string,
     RepositoryURL: string,
     ProjectURL: string,
-    Skills: string,
+    Tags: Array<string>,
     SkillsIcons: string
 }
+
+
+const Projects: Array<Project> = [
+    {
+        Title: "Mortis.",
+        Description: "A riddle inspired by Black Riddle...",
+        RepositoryURL: "https://github.com/FuryForev3r/mortis",
+        ProjectURL: "https://mortis.vercel.app",
+        Tags: ["next", "nextjs", "typescript", "ts", "vercel"],
+        SkillsIcons: "nextjs,ts,vercel"
+    },
+    {
+        Title: "Youtube-Forever.",
+        Description: "An API that downloads audio and gets information from Youtube videos!",
+        RepositoryURL: "https://github.com/FuryForev3r/Youtube-Forever",
+        ProjectURL: "https://youtubeforever.vercel.app",
+        Tags: ["express", "express", "typescript", "ts", "vercel"],
+        SkillsIcons: "express,ts,vercel"
+    },
+    {
+        Title: "Karess-Bot.",
+        Description: "This bot will tweet a random part of a song from a special playlist every 2 hours.",
+        RepositoryURL: "https://github.com/Furyforev3r/karess-bot",
+        ProjectURL: "https://twitter.com/karessbot",
+        Tags: ["python", "twitter"],
+        SkillsIcons: "python,twitter"
+    },
+    {
+        Title: "Dice-Roller-API.",
+        Description: "A dice rolling API done in Typescript using ExpressJS!",
+        RepositoryURL: "https://dice-roller-api-express-js.vercel.app/",
+        ProjectURL: "https://github.com/Furyforev3r/Dice-Roller-API-ExpressJS",
+        Tags: ["express", "express", "typescript", "ts", "vercel"],
+        SkillsIcons: "express,ts,vercel"
+    },
+    {
+        Title: "Decryptia.",
+        Description: "Decryptia is a project whose goal is to work with cryptography of all kinds.",
+        RepositoryURL: "https://github.com/Furyforev3r/Decryptia",
+        ProjectURL: "https://github.com/Furyforev3r/Decryptia",
+        Tags: ["python"],
+        SkillsIcons: "python"
+    },
+    {
+        Title: "Nextflow.",
+        Description: "A task app made in Next JS with drag and drop inspired by Trello!",
+        RepositoryURL: "https://github.com/Furyforev3r/Nextflow",
+        ProjectURL: "https://nextflow.vercel.app",
+        Tags: ["next", "nextjs", "typescript", "ts"],
+        SkillsIcons: "nextjs,ts"
+    },
+    {
+        Title: "To do App - Vite.",
+        Description: "A to-do list app made in Vite using ReactJS!",
+        RepositoryURL: "https://github.com/Furyforev3r/to-do-app-vite",
+        ProjectURL: "https://to-do-app-vite.vercel.app/",
+        Tags: ["vite", "typescript", "ts"],
+        SkillsIcons: "vite,ts"
+    },
+    {
+        Title: "File-Organizer.",
+        Description: "A simple file organizer.",
+        RepositoryURL: "https://github.com/Furyforev3r/File-Organizer",
+        ProjectURL: "https://github.com/Furyforev3r/File-Organizer",
+        Tags: ["python"],
+        SkillsIcons: "python"
+    },
+    {
+        Title: "Spotify-Now-Playing.",
+        Description: "This program checks and displays the current music you are listening to on Spotify!",
+        RepositoryURL: "https://github.com/Furyforev3r/Spotify-Now-Playing",
+        ProjectURL: "https://github.com/Furyforev3r/Spotify-Now-Playing",
+        Tags: ["python"],
+        SkillsIcons: "python"
+    },
+    {
+        Title: "",
+        Description: "",
+        RepositoryURL: "",
+        ProjectURL: "",
+        Tags: [""],
+        SkillsIcons: ""
+    },
+]
