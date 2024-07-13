249dc6215cb2cd57e5ec43459141e01b69902d90
FuryForev3r <messiasfuryforever@gmail.com>
2024-07-13 15:43:39 -0300

    ESLint formatting and updated projects.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ import Projects, { Project } from '@/services/projects'
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
       <main className={styles.main}>
         <section className={styles.presentation} id="about">
           <div className={styles.myInfo}>
             <h2>I'm <span><a href='https://github.com/FuryForev3r'>FuryForever!</a></span></h2>
-            <p>Hi! My internet alias is FuryForever, but my real name is Messias Robson. <br/> I am a Brazilian student and a Full Stack developer.</p>
+            <p>Hi! My internet alias is FuryForever, but my real name is Messias Robson. <br /> I am a Brazilian student and a Full Stack developer.</p>
             <div className={styles.socialLinks}>
               <ul>
                 <li>
@@ -85,33 +85,33 @@ export default function Home() {
           <h3 className={styles.sectionTitle} id="experiences"><a href="https://linkedin.com/in/furyforever">My experience</a></h3>
           <ul className={styles.experienceContainer}>
             <li className={styles.experience}>
-              <div className={styles.separator}/>
-                <div className={styles.experienceContent}>
-                  <div className={styles.experienceItem}>
-                    <h1>Freelancer Developer: Full Stack</h1>
-                    <p>As a freelance full stack developer, I've had the opportunity to work on a variety of projects for diverse clients, including numerous ventures abroad, with a significant focus on clients from Canada. From building responsive front-end interfaces to developing robust back-end solutions, my role encompasses the entire development lifecycle. With a focus on delivering high-quality, scalable code, I thrive in remote work environments, collaborating closely with clients to understand their needs and exceed their expectations. I pride myself on delivering top-notch results while fostering strong client relationships, ensuring seamless collaboration regardless of distance.</p>
-                  </div>
+              <div className={styles.separator} />
+              <div className={styles.experienceContent}>
+                <div className={styles.experienceItem}>
+                  <h1>Freelancer Developer: Full Stack</h1>
+                  <p>As a freelance full stack developer, I've had the opportunity to work on a variety of projects for diverse clients, including numerous ventures abroad, with a significant focus on clients from Canada. From building responsive front-end interfaces to developing robust back-end solutions, my role encompasses the entire development lifecycle. With a focus on delivering high-quality, scalable code, I thrive in remote work environments, collaborating closely with clients to understand their needs and exceed their expectations. I pride myself on delivering top-notch results while fostering strong client relationships, ensuring seamless collaboration regardless of distance.</p>
                 </div>
+              </div>
             </li>
             <li className={styles.experience}>
-              <div className={styles.separator}/>
-                <div className={styles.experienceContent}>
-                  <div className={styles.experienceItem}>
-                    <h1>Brazil Mobile School: Monitor/Trainee</h1>
-                    <p>As a student monitor at Brazil Mobile School, I took on the responsibility of assisting the school's students and facilitating learning across various concepts and technologies. From code reviews to guiding practical projects, I was there to provide technical support and encourage the progress of each student. My role also involved collaborating with instructors to ensure an effective and rewarding learning experience for all participants.</p>
-                  </div>
-                  <p className={styles.experienceDate}>2021-2022</p>
+              <div className={styles.separator} />
+              <div className={styles.experienceContent}>
+                <div className={styles.experienceItem}>
+                  <h1>Brazil Mobile School: Monitor/Trainee</h1>
+                  <p>As a student monitor at Brazil Mobile School, I took on the responsibility of assisting the school's students and facilitating learning across various concepts and technologies. From code reviews to guiding practical projects, I was there to provide technical support and encourage the progress of each student. My role also involved collaborating with instructors to ensure an effective and rewarding learning experience for all participants.</p>
                 </div>
+                <p className={styles.experienceDate}>2021-2022</p>
+              </div>
             </li>
             <li className={styles.experience}>
-              <div className={styles.separator}/>
-                <div className={styles.experienceContent}>
-                  <div className={styles.experienceItem}>
-                    <h1>Brazil Mobile School: Intern</h1>
-                    <p>As a full stack intern at Brazil Mobile School, I've had the invaluable opportunity to immerse myself in real-world development projects while refining my skills under expert guidance. From assisting in front-end development tasks to contributing to back-end functionalities, my role involves active participation in the entire software development process. Collaborating closely with senior developers and mentors, I've gained hands-on experience in applying theoretical knowledge to practical scenarios, ensuring my growth as a well-rounded professional in the field.</p>
-                  </div>
-                  <p className={styles.experienceDate}>2022-2024</p>
+              <div className={styles.separator} />
+              <div className={styles.experienceContent}>
+                <div className={styles.experienceItem}>
+                  <h1>Brazil Mobile School: Intern</h1>
+                  <p>As a full stack intern at Brazil Mobile School, I've had the invaluable opportunity to immerse myself in real-world development projects while refining my skills under expert guidance. From assisting in front-end development tasks to contributing to back-end functionalities, my role involves active participation in the entire software development process. Collaborating closely with senior developers and mentors, I've gained hands-on experience in applying theoretical knowledge to practical scenarios, ensuring my growth as a well-rounded professional in the field.</p>
                 </div>
+                <p className={styles.experienceDate}>2022-2024</p>
+              </div>
             </li>
           </ul>
         </section>
@@ -143,13 +143,13 @@ export default function Home() {
           <ul className={styles.appearancesContainer}>
             <li className={styles.appearance}>
               <a href='https://www.instagram.com/p/CzL-wEIOgD5/embed'>
-                <iframe id="frame" src="https://www.instagram.com/p/CzL-wEIOgD5/embed"/>
+                <iframe id="frame" src="https://www.instagram.com/p/CzL-wEIOgD5/embed" />
               </a>
             </li>
           </ul>
         </section>
       </main>
-      <Footer/>
+      <Footer />
     </>
   )
 }
--- src/services/projects.ts
+++ src/services/projects.ts
@@ -1,110 +1,86 @@
 export declare interface Project {
-    Title: string,
-    Description: string,
-    RepositoryURL: string,
-    ProjectURL: string,
-    Tags: Array<string>,
-    SkillsIcons: string
+  Title: string,
+  Description: string,
+  RepositoryURL: string,
+  ProjectURL: string,
+  Tags: Array<string>,
+  SkillsIcons: string
 }
 
 
 const Projects: Array<Project> = [
-    {
-        Title: "Mortis.",
-        Description: "A riddle inspired by Black Riddle...",
-        RepositoryURL: "https://github.com/FuryForev3r/mortis",
-        ProjectURL: "https://mortis.vercel.app",
-        Tags: ["next", "nextjs", "typescript", "ts", "vercel"],
-        SkillsIcons: "nextjs,ts,vercel"
-    },
-    {
-        Title: "Youtube-Forever.",
-        Description: "An API that downloads audio and gets information from Youtube videos!",
-        RepositoryURL: "https://github.com/FuryForev3r/Youtube-Forever",
-        ProjectURL: "https://youtubeforever.vercel.app",
-        Tags: ["express", "express", "typescript", "ts", "vercel"],
-        SkillsIcons: "express,ts,vercel"
-    },
-    {
-        Title: "Karess-Bot.",
-        Description: "This bot will tweet a random part of a song from a special playlist every 2 hours.",
-        RepositoryURL: "https://github.com/Furyforev3r/karess-bot",
-        ProjectURL: "https://twitter.com/karessbot",
-        Tags: ["python", "twitter"],
-        SkillsIcons: "python,twitter"
-    },
-    {
-        Title: "Dicer-API.",
-        Description: "A dice rolling API done in Typescript using ExpressJS!",
-        RepositoryURL: "https://github.com/Furyforev3r/Dicer-API",
-        ProjectURL: "https://dicer-api.vercel.app/",
-        Tags: ["express", "express", "typescript", "ts", "vercel"],
-        SkillsIcons: "express,ts,vercel"
-    },
-    {
-        Title: "Decryptia.",
-        Description: "Decryptia is a project whose goal is to work with cryptography of all kinds.",
-        RepositoryURL: "https://github.com/Furyforev3r/Decryptia",
-        ProjectURL: "https://github.com/Furyforev3r/Decryptia",
-        Tags: ["python"],
-        SkillsIcons: "python"
-    },
-    {
-        Title: "Nextflow.",
-        Description: "A task app made in Next JS with drag and drop inspired by Trello!",
-        RepositoryURL: "https://github.com/Furyforev3r/Nextflow",
-        ProjectURL: "https://nextflow.vercel.app",
-        Tags: ["next", "nextjs", "typescript", "ts"],
-        SkillsIcons: "nextjs,ts"
-    },
-    {
-        Title: "To do App - Vite.",
-        Description: "A to-do list app made in Vite using ReactJS!",
-        RepositoryURL: "https://github.com/Furyforev3r/to-do-app-vite",
-        ProjectURL: "https://to-do-app-vite.vercel.app/",
-        Tags: ["vite", "typescript", "ts"],
-        SkillsIcons: "vite,ts"
-    },
-    {
-        Title: "File-Organizer.",
-        Description: "A simple file organizer.",
-        RepositoryURL: "https://github.com/Furyforev3r/File-Organizer",
-        ProjectURL: "https://github.com/Furyforev3r/File-Organizer",
-        Tags: ["python"],
-        SkillsIcons: "python"
-    },
-    {
-        Title: "Spotify-Now-Playing.",
-        Description: "This program checks and displays the current music you are listening to on Spotify!",
-        RepositoryURL: "https://github.com/Furyforev3r/Spotify-Now-Playing",
-        ProjectURL: "https://github.com/Furyforev3r/Spotify-Now-Playing",
-        Tags: ["python"],
-        SkillsIcons: "python"
-    },
-    {
-        Title: "StockWise.",
-        Description: "A stock management REST API.",
-        RepositoryURL: "https://github.com/Furyforev3r/StockWise",
-        ProjectURL: "https://github.com/Furyforev3r/StockWise",
-        Tags: ["typescript", "firebase", "expressjs"],
-        SkillsIcons: "ts,firebase,expressjs"
-    },
-    {
-        Title: "FutDraw.",
-        Description: "A program that randomizes the draw of teams in a soccer competition.",
-        RepositoryURL: "https://github.com/Furyforev3r/FutDraw",
-        ProjectURL: "https://github.com/Furyforev3r/FutDraw",
-        Tags: ["python"],
-        SkillsIcons: "python"
-    },
-    {
-        Title: "DiscordTmpl.",
-        Description: "A quick discord bot template creator.",
-        RepositoryURL: "https://github.com/Furyforev3r/DiscordTmpl",
-        ProjectURL: "https://github.com/Furyforev3r/DiscordTmpl",
-        Tags: ["cs", "csharp", "c#", "discord"],
-        SkillsIcons: "cs"
-    }
+  {
+    Title: "Nextflow.",
+    Description: "A task app made in Next JS with drag and drop inspired by Trello!",
+    RepositoryURL: "https://github.com/Furyforev3r/Nextflow",
+    ProjectURL: "https://nextflow.vercel.app",
+    Tags: ["next", "nextjs", "typescript", "ts"],
+    SkillsIcons: "nextjs,ts"
+  },
+  {
+    Title: "Mortis.",
+    Description: "A riddle inspired by Black Riddle...",
+    RepositoryURL: "https://github.com/FuryForev3r/mortis",
+    ProjectURL: "https://mortis.vercel.app",
+    Tags: ["next", "nextjs", "typescript", "ts", "vercel"],
+    SkillsIcons: "nextjs,ts,vercel"
+  },
+  {
+    Title: "Youtube-Forever.",
+    Description: "An API that downloads audio and gets information from Youtube videos!",
+    RepositoryURL: "https://github.com/FuryForev3r/Youtube-Forever",
+    ProjectURL: "https://youtubeforever.vercel.app",
+    Tags: ["express", "express", "typescript", "ts", "vercel"],
+    SkillsIcons: "express,ts,vercel"
+  },
+  {
+    Title: "Dicer-API.",
+    Description: "A dice rolling API!",
+    RepositoryURL: "https://github.com/Furyforev3r/Dicer-API",
+    ProjectURL: "https://dicer-api.vercel.app/",
+    Tags: ["express", "express", "typescript", "ts", "vercel"],
+    SkillsIcons: "express,ts,vercel"
+  },
+  {
+    Title: "Decryptia.",
+    Description: "Decryptia is a project whose goal is to work with cryptography of all kinds.",
+    RepositoryURL: "https://github.com/Furyforev3r/Decryptia",
+    ProjectURL: "https://github.com/Furyforev3r/Decryptia",
+    Tags: ["python"],
+    SkillsIcons: "python"
+  },
+  {
+    Title: "Spotify-Now-Playing.",
+    Description: "This program checks and displays the current music you are listening to on Spotify!",
+    RepositoryURL: "https://github.com/Furyforev3r/Spotify-Now-Playing",
+    ProjectURL: "https://github.com/Furyforev3r/Spotify-Now-Playing",
+    Tags: ["python"],
+    SkillsIcons: "python"
+  },
+  {
+    Title: "StockWise.",
+    Description: "A stock management REST API.",
+    RepositoryURL: "https://github.com/Furyforev3r/StockWise",
+    ProjectURL: "https://github.com/Furyforev3r/StockWise",
+    Tags: ["typescript", "firebase", "expressjs"],
+    SkillsIcons: "ts,firebase,expressjs"
+  },
+  {
+    Title: "FutDraw.",
+    Description: "A program that randomizes the draw of teams in a soccer competition.",
+    RepositoryURL: "https://github.com/Furyforev3r/FutDraw",
+    ProjectURL: "https://github.com/Furyforev3r/FutDraw",
+    Tags: ["python"],
+    SkillsIcons: "python"
+  },
+  {
+    Title: "DiscordTmpl.",
+    Description: "A quick discord bot template creator.",
+    RepositoryURL: "https://github.com/Furyforev3r/DiscordTmpl",
+    ProjectURL: "https://github.com/Furyforev3r/DiscordTmpl",
+    Tags: ["cs", "csharp", "c#", "discord"],
+    SkillsIcons: "cs"
+  }
 ]
 
 export default Projects
